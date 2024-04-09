39a9e8429ffb482f6081da693c318d5217fbdbf1
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-04-09 11:04:16 +0200

    Modified cypress.config file

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+export default {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    video: false,
+  },
+};
